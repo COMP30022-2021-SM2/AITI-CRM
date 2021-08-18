6f7821433b74eb936c4434c26c1f826b978f20e0
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-08-18 14:22:14 +0800

    Update customer.js

---
+++ src/models/customer.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const Order = require('order')
+
+// Customer model
+const customerSchema = new mongoose.Schema({
+    firstName: { type: String, require: true },
+    lastName: { type: String, require: true },
+    emailAddress: { type: String, require: true, unique: true },
+    phoneNumber: { type: Number, require: true, unique: true},
+    companyName: { type: String, require: true },
+    address: { type: addressSchema, required: true },
+    abn: { type: Number, require: true, unique: true },
+    notes : String
+})
+
+const addressSchema = new mongoose.Schema({
+    _id: false,
+    addressLine1: { type: String, required: true },
+    addressLine2: String,
+    postcode: { type: Number, required: true },
+    suburb: { type: String, required: true },
+    state: { type: String, required: true}
+})
+
+const Customer = mongoose.model('Customer', customerSchema)
+module.exports = Customer
