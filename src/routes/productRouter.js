fb445f0ba7f5cfa20fb945d9a7b29c1002b2067e
Cathy-kk <yu572398517@gmail.com>
2021-08-23 14:26:58 +0800

    fix bug

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,3 +3,5 @@ const productController = require("../controllers/productController")
 const productRouter = express.Router()
 
 productRouter.post('/add-product', productController.addProduct)
+
+module.exports = productRouter;
