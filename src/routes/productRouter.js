b791f915aa440b1157549c7b1292c31ecdd66a3c
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-08-24 15:30:40 +0800

    Add authentication middleware

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const productController = require("../controllers/productController")
+const utilities = require('./utility')
 const productRouter = express.Router()
 
-productRouter.post('/add-product', productController.addProduct)
+productRouter.post('/add-product', utilities.isLoggedIn, productController.addProduct)
 
-productRouter.post('/edit/:tag', productController.updateProduct)
+productRouter.post('/edit/:tag', utilities.isLoggedIn, productController.updateProduct)
 
 module.exports = productRouter;
