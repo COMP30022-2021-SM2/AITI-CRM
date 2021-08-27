936f36550ce413240a6cf3f32545456e60cbae86
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-08-27 20:55:51 +0800

    Add comment

--- root/server/routes/userRouter.js
+++ root/server/routes/userRouter.js
@@ -4,12 +4,9 @@ const utilities = require('./utility')
 require('../config/passport')(passport)
 const userRouter = express.Router()
 
+// Login
 userRouter.get('/login',utilities.isNotLoggedIn, (req, res) => {
     res.send('you are trying to login')
-})
-
-userRouter.get('/signup', utilities.isNotLoggedIn, (req, res) => {
-    res.send('you are trying to create an new account')
 })
 
 userRouter.post('/login', utilities.isNotLoggedIn, passport.authenticate('local-login', {
@@ -18,18 +15,24 @@ userRouter.post('/login', utilities.isNotLoggedIn, passport.authenticate('local-
     failureFlash: true // allow flash messages
 }))
 
+// Signup
+userRouter.get('/signup', utilities.isNotLoggedIn, (req, res) => {
+    res.send('you are trying to create an new account')
+})
+
 userRouter.post('/signup', utilities.isNotLoggedIn, passport.authenticate('local-signup', {
     successRedirect: '/login', // redirect to the homepage
     failureRedirect: '/signup', // redirect to signup page
     failureFlash: true // allow flash messages
 }))
 
-// LOGOUT
+// Logout
 userRouter.post('/logout', function(req, res) {
     // save the favourites
     req.logout();
     console.log('logout successfully')
     res.redirect('/login');
 });
+
 
 module.exports = userRouter
