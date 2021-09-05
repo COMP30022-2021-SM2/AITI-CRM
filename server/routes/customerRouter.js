1b7cce134f6b07654fc68ce8729bb6b2a148cbf5
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-09-05 22:33:26 +0800

    Delete season authentication

--- server/routes/customerRouter.js
+++ server/routes/customerRouter.js
@@ -4,6 +4,6 @@ const utilities = require('./utility')
 const customerRouter = express.Router()
 
 // Add new customer
-customerRouter.post('/customer/add-customer', utilities.isLoggedIn, customerController.addCustomer)
+customerRouter.post('/customer/add-customer', customerController.addCustomer)
 
 module.exports = customerRouter;
