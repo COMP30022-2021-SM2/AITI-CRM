508e64570824d04c81be1867cbcc88c8e2f84067
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-10-01 22:52:10 +0800

    Add support for netlify origins

--- server/app.js
+++ server/app.js
@@ -23,9 +23,21 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+let allowedOrigins = ['http://localhost:3000', 'https://aiti-crm.netlify.app'];
+
 app.use(cors({
     credentials: true, // add Access-Control-Allow-Credentials to header
-    origin: "http://localhost:3000"
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            let msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 
 // setup a session store signing the contents using the secret key
