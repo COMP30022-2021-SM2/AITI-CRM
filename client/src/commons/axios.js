d23495ee571d69c48a28e82dac4aa88592b83b42
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-10-10 21:59:49 +0800

    Add space

--- client/src/commons/axios.js
+++ client/src/commons/axios.js
@@ -3,7 +3,7 @@ import _axios from 'axios';
 // Having the online Heroku link with our web app and the localhost connection
 const axios = (baseUrl) => {
   const instance = _axios.create({
-    //baseURL: 'http://localhost:5000'
+    // baseURL: 'http://localhost:5000'
     baseURL: 'https://aiti-crm-server.herokuapp.com'
   });
   return instance;
