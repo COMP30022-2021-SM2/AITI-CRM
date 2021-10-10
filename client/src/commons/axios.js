471a7ab43450dadb8097435b7edee2c055ca7ae8
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2021-10-10 21:56:35 +0800

    Changed baseURL to heroku

--- client/src/commons/axios.js
+++ client/src/commons/axios.js
@@ -3,8 +3,8 @@ import _axios from 'axios';
 // Having the online Heroku link with our web app and the localhost connection
 const axios = (baseUrl) => {
   const instance = _axios.create({
-    baseURL: 'http://localhost:5000'
-    // baseURL: 'https://aiti-crm-server.herokuapp.com'
+    //baseURL: 'http://localhost:5000'
+    baseURL: 'https://aiti-crm-server.herokuapp.com'
   });
   return instance;
 };
