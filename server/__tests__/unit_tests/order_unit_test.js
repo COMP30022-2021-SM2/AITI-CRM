e596db94278bbf042590f8e87fa81b7ca94b126c
yifeiwangG100 <yifeiandywang@outlook.com>
2021-10-22 12:20:46 +1100

    change test file names

--- server/__tests__/unit_tests/order_unit_test.js
+++ server/__tests__/unit_tests/order_unit_test.js
@@ -1,18 +1,18 @@
-const orderController = require('../controllers/orderController');
-const Order = require('../models/order');
+const orderController = require('../../controllers/orderController');
+const Order = require('../../models/order');
 const ObjectId = require('mongoose').Types.ObjectId;
 
-describe('customerController', function() {
+describe('customerController', function () {
     const res = {
         json: jest.fn(),
-        status: function(s) {this.statusCode = s; return this;}
-}
+        status: function (s) { this.statusCode = s; return this; }
+    }
 
     //  properties needed for request to test
     //  openForBusiness
     const req = {
-        params: {orderId: "61669663d11bdfa9fe7588e8"},
-        body: {status: "completed"}
+        params: { orderId: "61669663d11bdfa9fe7588e8" },
+        body: { status: "completed" }
     };
 
     beforeAll(() => {
@@ -23,40 +23,40 @@ describe('customerController', function() {
             {
                 _id: "61669663d11bdfa9fe7588e8",
                 status: "completed"
-                
+
             }
         ]);
 
         Order.findOne = jest.fn().mockResolvedValue(
             {
                 _id: "61669663d11bdfa9fe7588e8"
-                
+
             }
         );
-        
+
         // mock lean() method
         Order.updateOne.mockImplementationOnce(() => ({
             lean: jest.fn().mockReturnValue(
                 {
-                   _id: "61669663d11bdfa9fe7588e8",
-                   status: "completed"
+                    _id: "61669663d11bdfa9fe7588e8",
+                    status: "completed"
                 }),
-                
-            }));
-        
+
+        }));
+
         Order.findOne.mockImplementationOnce(() => ({
-           lean: jest.fn().mockReturnValue(
+            lean: jest.fn().mockReturnValue(
                 {
                     _id: "61669663d11bdfa9fe7588e8"
                 }),
-                    
-            }));
+
+        }));
 
         // call the controller function before testing various properties of
         // it
         orderController.updateOrderStatus(req, res);
-        jest.spyOn(console, 'log').mockImplementation(() => {});
-      });
+        jest.spyOn(console, 'log').mockImplementation(() => { });
+    });
 
     afterAll(() => {
         // Restore mock after all tests are done, so it won't affect other test suites
@@ -68,10 +68,10 @@ describe('customerController', function() {
     });
 
     // Test 1: test the console
-    test("Test 1: test console log, expecting to be order is updated successfully", ()=>{
+    test("Test 1: test console log, expecting to be order is updated successfully", () => {
         expect(console.log).toBeCalledTimes(1);
         expect(console.log).toHaveBeenLastCalledWith('Update order successfully')
-     });
+    });
 
     test("Test 2: testing the order update successfully, expecting return json object ", () => {
         // when I run the controller, I expect that the json method will
@@ -79,5 +79,5 @@ describe('customerController', function() {
         expect(res.json.mock.calls.length).toEqual(1);
         expect(res.statusCode).toBe(200);
 
-      });
-    })
+    });
+})
